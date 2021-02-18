07a0157f553d0ce4e4242b943ce376c9939b6f25
sarahstandish <z.sisyphusrocks@gmail.com>
2021-02-18 08:20:36 -0800

    changes to styling

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 
-$google_api_key = 'AIzaSyAr7uAWzNf62vbQYuX8kiJULqgQ01JsErE';
-$flickr_api_key = '7441a4ff83fda0c1e35cbd7c4016210f';
-
+include 'keys.php';
 
 function get_lat_and_lng($google_api_key, $location) {
 
