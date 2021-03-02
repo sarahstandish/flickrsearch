c94a543136fe791e90f2b50def5ee52dd8da2c8c
sarahstandish <z.sisyphusrocks@gmail.com>
2021-03-02 11:44:36 -0800

    improved appearance of check boxes

--- index.php
+++ index.php
@@ -58,19 +58,62 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <p class="explanation">If specified, will return photos geotagged to within roughly 30km of the city center.</p>
             <span class="error"><?php echo $location_error; ?></span>
         <label>Licenses</label>
-            <ul>
-                <li><input type='checkbox' name="licenses[]" value="0" <?php echo defaultUnchecked(0) ?>>  All Rights Reserved  </li>
-                <li><input type='checkbox'  name="licenses[]" value="1" <?php echo defaultUnchecked(1) ?>>  Attribution-NonCommercial-ShareAlike License  <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="2" <?php echo defaultUnchecked(2) ?>>  Attribution-NonCommercial License  <a href="https://creativecommons.org/licenses/by-nc/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="3" <?php echo defaultUnchecked(3) ?>>  Attribution-NonCommercial-NoDerivs License  <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="4" <?php echo defaultChecked(4) ?>>  Attribution License  <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="5" <?php echo defaultChecked(5) ?>>  Attribution-ShareAlike License  <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="6" <?php echo defaultUnchecked(6) ?>>  Attribution-NoDerivs License  <a href="https://creativecommons.org/licenses/by-nd/2.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="7" <?php echo defaultChecked(7) ?>>  No known copyright restrictions  <a href="https://www.flickr.com/commons/usage/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="8" <?php echo defaultChecked(8) ?>>  United States Government Work  <a href="http://www.usa.gov/copyright.shtml" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="9" <?php echo defaultChecked(9) ?>>  Public Domain Dedication (CC0)  <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">Learn more</a></li>
-                <li><input type='checkbox' name="licenses[]" value="10" <?php echo defaultChecked(10) ?>>  Public Domain Mark  <a href="https://creativecommons.org/publicdomain/mark/1.0/" target="_blank">Learn more</a></li>
-            </ul>
+            <div class='licenses'>
+
+            <label class="container">All Rights Reserved
+                <input type='checkbox' name="licenses[]" value="0" <?php echo defaultUnchecked(0) ?>>
+                <span class="checkmark"></span>
+            </label>
+            
+            <label class="container">Attribution-NonCommercial-ShareAlike License  <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox'  name="licenses[]" value="1" <?php echo defaultUnchecked(1) ?>>
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Attribution-NonCommercial License  <a href="https://creativecommons.org/licenses/by-nc/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="2" <?php echo defaultUnchecked(2) ?>>
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Attribution-NonCommercial-NoDerivs License  <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="3" <?php echo defaultUnchecked(3) ?>>
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Attribution License  <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="4" <?php echo defaultChecked(4) ?>>  
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Attribution-ShareAlike License  <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="5" <?php echo defaultChecked(5) ?>>
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Attribution-NoDerivs License  <a href="https://creativecommons.org/licenses/by-nd/2.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="6" <?php echo defaultUnchecked(6) ?>>  
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">No known copyright restrictions  <a href="https://www.flickr.com/commons/usage/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="7" <?php echo defaultChecked(7) ?>>  
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">United States Government Work  <a href="http://www.usa.gov/copyright.shtml" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="8" <?php echo defaultChecked(8) ?>>  
+                <span class="checkmark"></span>
+            </label>
+
+            <label class="container">Public Domain Dedication (CC0)  <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">Learn more</a>
+                <input type='checkbox' name="licenses[]" value="9" <?php echo defaultChecked(9) ?>>  
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <label class="container">Public Domain Mark  <a href="https://creativecommons.org/publicdomain/mark/1.0/" target="_blank">Learn more</a>
+            <input type='checkbox' name="licenses[]" value="10" <?php echo defaultChecked(10) ?>>  
+            <span class="checkmark"></span>
+        </label>
         <p class="explanation">Default selection will return images licensed for commercial use and modifications.</p>
         <span class="error"><?php echo $license_error; ?></span>
         <label>Minimum DPI</label>
